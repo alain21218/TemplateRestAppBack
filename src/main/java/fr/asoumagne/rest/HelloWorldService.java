9503d1bc0e4203596f476dfd9fb7ee72a8b64b82
unknown <alain21218@gmail.com>
2018-02-02 23:08:00 +0100

    add two simple routes

--- src/main/java/fr/asoumagne/rest/HelloWorldService.java
+++ src/main/java/fr/asoumagne/rest/HelloWorldService.java
@@ -1,27 +1,68 @@
 package fr.asoumagne.rest;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/users")
 public class HelloWorldService {
 	
-	String[] datas = new String[]{"John", "Kevin", "Tom"};
-
 	@GET
-	@Path("/{num}")
-	public Response getMsg(@PathParam("num") int num) {
-		//Error
-		if(num >= datas.length || num < 0){
-			return Response.status(404).build();
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public User getUser( @PathParam("id") int id ) {
+        User user = new User(id, "John Doe", 22);
+        return user;
+    }
+	
+	@GET
+    @Path("/test/{name}")
+    public int getMsg( @PathParam("name") int name ) {
+		System.out.println(name);
+        return name;
+    }
+	
+	public class User{
+		int id;
+		String name;
+		int age;
+		
+		public User(int id, String name, int age) {
+			this.id = id;
+			this.name = name;
+			this.age = age;
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public int getAge() {
+			return age;
+		}
+
+		public void setAge(int age) {
+			this.age = age;
 		}
 		
-		//Done
-		String output = "Hello " + datas[num];
-		return Response.status(200).entity(output).build();
-
+		
+		
 	}
 
 }
